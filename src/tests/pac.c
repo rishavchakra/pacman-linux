f2bb3bc890564397daad480005b45aa100e924aa
rishavchakra <rishavchakra@gmail.com>
2024-07-19 00:40:15 -0500

    Better PAC testing

--- src/tests/pac.c
+++ src/tests/pac.c
@@ -17,18 +17,28 @@ int test_fn(void);
 int main() {
   {
     printf("Data pointer signing\n");
-    void *buf = malloc(10);
+    char *buf = malloc(10);
+    buf[0] = 'w';
     uint64_t salt = PAC_SALT;
+
+    // The data pointer works before any signing
+    printf("Extracted data %c\n", buf[0]);
 
     printf("Pointer before signing:\n%p\n", buf);
 
     asm("pacda %[ptr], %[salt]" : [ptr] "+r"(buf) : [salt] "r"(salt));
+
+    // This will segfault!
+    // The data pointer is signed and not yet authenticated
+    // printf("Extracted data %c\n", buf[0]);
 
     printf("Pointer after signing:\n%p\n", buf);
 
     asm("autda %[ptr], %[salt]" : [ptr] "+r"(buf) : [salt] "r"(salt));
 
     printf("Pointer after authenticating:\n%p\n", buf);
+
+    printf("Extracted data %c\n", buf[0]);
 
     printf("Data pointer authentication successful!\n");
   }
@@ -40,15 +50,24 @@ int main() {
     int (*fn_ptr)() = test_fn;
     uint64_t salt = PAC_SALT;
 
+    // The instruction pointer works before any signing
+    test_fn();
+
     printf("Pointer before signing:\n%p\n", fn_ptr);
 
     asm("pacia %[ptr], %[salt]" : [ptr] "+r"(fn_ptr) : [salt] "r"(salt));
 
     printf("Pointer after signing:\n%p\n", fn_ptr);
 
+    // This will segfault!
+    // The instr pointer is signed and not yet authenticated
+    // test_fn();
+
     asm("autia %[ptr], %[salt]" : [ptr] "+r"(fn_ptr) : [salt] "r"(salt));
 
     printf("Pointer after authenticating:\n%p\n", fn_ptr);
+
+    test_fn();
 
     printf("Instruction pointer authentication successful!\n");
   }
